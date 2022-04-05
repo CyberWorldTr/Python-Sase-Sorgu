6eca8327405a8193af6b87451606fb6e91deb001
CyberWorldTr <cyberworldtur@gmail.com>
2022-04-05 05:24:15 +0300

    first commit

---
+++ sorgu.py
@@ -0,0 +1,10 @@
+from vininfo import Vin
+
+x = Vin('MAJGERTYKGHG56037')    
+
+print(x.country)              
+print(x.manufacturer)      
+print(x.wmi)                      
+print(x.vds)
+print(x.vis)
+print(x.region)
